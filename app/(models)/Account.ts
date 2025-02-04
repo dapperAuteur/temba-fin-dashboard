1d8d44a28c92bc78b144344fd0667fe453e04336
awe <aweful@awews.com>
2025-02-03 21:01:08 -0500

    refactored to use a typescript model for accounts

--- app/(models)/Account.ts
+++ app/(models)/Account.ts
@@ -1,10 +1,18 @@
-import { Schema, model, models, Types } from 'mongoose';
+import { Schema, model, models, Types, Document } from 'mongoose';
 
-const AccountSchema = new Schema({
+export interface IAccount extends Document {
+  name: string;
+  type: string;
+  balance: number;
+  userId: Types.ObjectId;
+  tags: Types.ObjectId[];
+}
+
+const AccountSchema = new Schema<IAccount>({
   name: { type: String, required: true },
   type: { type: String, required: true },
   balance: { type: Number, required: true },
-  userId: { type: Types.ObjectId, required: true },
+  userId: { type: Types.ObjectId, required: true, ref: "User" }, // Reference to the User model
+  tags: [{ type: Types.ObjectId, ref: "Tag" }], // Optional: If you have a Tag model
 });
-
-export default models.Account || model('Account', AccountSchema);
+export default models.Account || model<IAccount>('Account', AccountSchema);
