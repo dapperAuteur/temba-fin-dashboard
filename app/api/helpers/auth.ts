5558ce0498fd96028b305ee66cf56cc779416362
awe <aweful@awews.com>
2025-02-03 21:04:07 -0500

    refactored auth functions to be typescript
    function to confirm user is authenticated and authorized to perform the action on the api

--- app/api/helpers/auth.ts
+++ app/api/helpers/auth.ts
@@ -1,8 +1,9 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import Account from "@/app/(models)/Account";
 
 // Check if the user is signed in
-export const isAuthenticated = async (req) => {
+export const isAuthenticated = async (req: Request) => {
   const session = await getServerSession(authOptions);
   if (!session) {
     throw new Error("Unauthorized: User not logged in.");
@@ -11,7 +12,7 @@ export const isAuthenticated = async (req) => {
 };
 
 // Check if the user is the owner of the account
-export const isOwner = async (accountId, userId) => {
+export const isOwner = async (accountId: string, userId: string) => {
   const account = await Account.findOne({ _id: accountId, userId });
   if (!account) {
     throw new Error("Forbidden: You do not have permission to modify this account.");
