8053022bdd0c74c5035297a7284ea1f452296ec4
awe <aweful@awews.com>
2025-02-08 22:57:48 -0500

    refactor to a cleaner TypeScript implementation of dbConnect.ts
    cleaner TypeScript implementation of dbConnect.ts without using any

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
-declare global {
-  var mongoose: any;
+
+interface CachedConnection {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
 }
 
 const MONGODB_URI = process.env.MONGODB_URI!;
@@ -9,42 +11,34 @@ if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
 }
 
-let cached = global.mongoose;
+const cached: CachedConnection = {
+  conn: null,
+  promise: null
+};
 
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null
-  };
-}
-
-async function dbConnect() {
-  console.log("dbConnect");
-  
+async function dbConnect(): Promise<typeof mongoose> {
   if (cached.conn) {
-    console.log("cached");
     return cached.conn;
   }
+
   if (!cached.promise) {
-    console.log("!cached.promise");
     const opts = {
       bufferCommands: false,
     };
+
     cached.promise = mongoose.connect(MONGODB_URI, opts)
       .then((mongoose) => {
-        console.log("mongoose connected");
-        
         return mongoose;
       });
   }
+
   try {
-    console.log("cached.conn");
     cached.conn = await cached.promise;
   } catch (e) {
     cached.promise = null;
-    console.log('38 ./lib/dbConnect.ts e :>> ', e);
     throw e;
   }
+
   return cached.conn;
 }
 
