fa77edc102f7c6a5a2099b7a3df857f56ba1ac2e
awe <aweful@awews.com>
2025-02-23 01:10:35 -0500

    changed import to use user type instead of auth type

--- app/hooks/modelHooks.ts
+++ app/hooks/modelHooks.ts
@@ -4,7 +4,7 @@ import { createDataHook } from './useData'
 import type { IAccount } from './../../types/accounts'
 import { ITag } from '@/types/tags';
 import type { ITransaction } from '@/types/transactions'
-import type { IUser } from '@/types/auth'
+import type { IUser } from '@/types/users'
 
 export const useAccounts = createDataHook<IAccount[]>('accounts')
 export const useTags = createDataHook<ITag[]>('tags')
